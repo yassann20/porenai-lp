43a84275779ae12d720524c4f2a814d09b024e59
yassann20 <kaiseif4e@gmail.com>
2024-10-23 23:18:18 +0900

    画像LPへ改修

---
+++ porenai-lp/action.js
@@ -0,0 +1,6 @@
+$(function(){
+        $('.sp-hum').on('click', function(){
+        $(this).children('span').toggleClass('active');
+            $('.nav').toggleClass('active');
+        });
+    });
